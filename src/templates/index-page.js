5efd807cc9329d985eb4bdaa1326290d47b5e26a
severi.kupari <severi.kupari@ropocapital.fi>
2021-01-23 00:26:18 +0200

    Chanegs to logo and some cleaning

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -90,18 +90,18 @@ export const IndexPageTemplate = ({
                 <div className="columns">
                   <div className="column is-12 has-text-centered">
                     <Link className="btn" to="/products">
-                      See all products
+                      Tutustu osaamiseemme!
                     </Link>
                   </div>
                 </div>
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
+                    Viimeisimmät uutiset
                   </h3>
                   <BlogRoll />
                   <div className="column is-12 has-text-centered">
                     <Link className="btn" to="/blog">
-                      Read more
+                      Lue lisää
                     </Link>
                   </div>
                 </div>
