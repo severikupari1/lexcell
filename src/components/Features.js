863f8599abad0ea2285b54fe32fac52cb626aeae
severi.kupari <severi.kupari@ropocapital.fi>
2021-01-23 19:01:50 +0200

    Css changes with krista

--- src/components/Features.js
+++ src/components/Features.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 import { HTMLContent } from "./Content";
+import MarkdownContent from "./MardownContent";
 
 const FeatureGrid = ({ gridItems }) => (
   <div className="columns is-multiline">
@@ -18,7 +19,7 @@ const FeatureGrid = ({ gridItems }) => (
               <PreviewCompatibleImage imageInfo={item} />
             </div>
           </div>
-          <HTMLContent content={item.text}></HTMLContent>
+          <MarkdownContent content={item.text}></MarkdownContent>
         </section>
       </div>
     ))}
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -40,8 +40,8 @@ export const IndexPageTemplate = ({
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
             boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+              'rgb(129, 0, 0) 0.5rem 0px 0px, rgb(129, 0, 0) -0.5rem 0px 0px',
+            backgroundColor: 'rgb(129 0 0)',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
@@ -50,11 +50,11 @@ export const IndexPageTemplate = ({
           {title}
         </h1>
         <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
+          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen has-text-centered"
           style={{
             boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+              'rgb(129, 0, 0) 0.5rem 0px 0px, rgb(129, 0, 0) -0.5rem 0px 0px',
+            backgroundColor: 'rgb(129, 0, 0)',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
