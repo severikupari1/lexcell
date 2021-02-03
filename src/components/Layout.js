bafb5a217eed169c0e020b7b89a3466b14fd86a2
severi.kupari <severi.kupari@ropocapital.fi>
2021-02-03 22:11:59 +0200

    Update og things

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -47,7 +47,7 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:type" content="website" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        <meta property="og:image" content="/img/og-image.jpg" />
+        <meta property="og:image" content="https://lexcell.netlify.app/img/og-image.jpg" />
 
 
         <meta name="twitter:card" content="summary_large_image" />
@@ -55,7 +55,7 @@ const TemplateWrapper = ({ children }) => {
         <meta property="twitter:url" content="https://lexcell.netlify.app/" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content="/img/og-image.jpg" />
+        <meta name="twitter:image" content="https://lexcell.netlify.app/img/og-image.jpg" />
 
         <meta
           property="og:image"
