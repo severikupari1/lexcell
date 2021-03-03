c2daf6ec95c615c6007fd0143071e2c90e8ac892
severi.kupari@ropocapital.fi <severi.kupari@ropocapital.fi>
2021-03-03 22:13:39 +0200

    Lighthouse report better scores!

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -59,9 +59,9 @@ export const IndexPageTemplate = ({
 
                 <div className="columns">
                   <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2 text-wrap">
+                    <h2 className="has-text-weight-semibold is-size-2 text-wrap">
                       {heading}
-                    </h3>
+                    </h2>
                     <p>{description}</p>
                   </div>
                 </div>
