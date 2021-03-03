da359b34fdc703981776a242c1ecbd0bca3835fd
severi.kupari@ropocapital.fi <severi.kupari@ropocapital.fi>
2021-03-03 22:20:40 +0200

    Cms uninstall fix

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,4 @@
 import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare'
-import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
