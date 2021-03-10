9eb4238cf589e98e2460dd6c98a9411a638ffe21
severi.kupari@ropocapital.fi <severi.kupari@ropocapital.fi>
2021-03-10 09:26:12 +0200

    Added tawk.io button to gastby browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,13 @@
-import "./src/styles/global.css"
+require("./src/styles/global.css")
+
+exports.onInitialClientRender = () => {
+    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+    (function () {
+        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/603fcfd91c1c2a130d649043/1evsk7qgq';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
+}
