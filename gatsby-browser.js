f14f49a9fb18516aaa369b880f79b94bfed095e8
severi.kupari@ropocapital.fi <severi.kupari@ropocapital.fi>
2021-03-15 21:59:17 +0200

    test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,18 +1,1 @@
 require("./src/styles/global.css")
-
-exports.onRouteUpdate = () => {
-    // Direct Chat Link
-    // https://tawk.to/chat/tawkToPropertyId/tawkToKey
-    
-    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-    (function () {
-        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-        s1.async = true;
-        s1.src = 'https://embed.tawk.to/603fcfd91c1c2a130d649043/1evsk7qgq';
-        s1.charset = 'UTF-8';
-        s1.setAttribute('crossorigin', '*');
-        s0.parentNode.insertBefore(s1, s0);
-        console.log("Chat loaded!")
-    })();
-
-}
